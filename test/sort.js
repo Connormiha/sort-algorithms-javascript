db20e5d6bef054f6198aaa412b136602d693e5a3
Mihail Bodrov <connormiha1@gmail.com>
2016-04-12 22:38:03 +0300

    Change expect syntax

--- test/sort.js
+++ test/sort.js
@@ -18,39 +18,39 @@ for (let item of tests) {
             let arr = [1, 5, 9, 12, -10, 0, 15, 20, 100, 4];
             let arrCorrect = [-10, 0, 1, 4, 5, 9, 12, 15, 20, 100];
 
-            expect(sort(arr)).eql(arrCorrect);
+            expect(sort(arr)).to.eql(arrCorrect);
         });
 
         it('Should sort empty array', ()=> {
-            expect(sort([])).eql([]);
+            expect(sort([])).to.eql([]);
         });
 
         it('Should sort one element array', ()=> {
-            expect(sort([1])).eql([1]);
+            expect(sort([1])).to.eql([1]);
         });
 
         it('Should sort [2, 1] array', ()=> {
-            expect(sort([2, 1])).eql([1, 2]);
+            expect(sort([2, 1])).to.eql([1, 2]);
         });
 
         it('Should sort [1, 0, 0, 0]', ()=> {
-            expect(sort([1, 0, 0, 0])).eql([0, 0, 0, 1]);
+            expect(sort([1, 0, 0, 0])).to.eql([0, 0, 0, 1]);
         });
 
         it('Should sort [1, 0, 0, 1]', ()=> {
-            expect(sort([1, 0, 0, 1])).eql([0, 0, 1, 1]);
+            expect(sort([1, 0, 0, 1])).to.eql([0, 0, 1, 1]);
         });
 
         it('Should sort [0, 0, 0, 1]', ()=> {
-            expect(sort([0, 0, 0, 1])).eql([0, 0, 0, 1]);
+            expect(sort([0, 0, 0, 1])).to.eql([0, 0, 0, 1]);
         });
 
         it('Should sort [0, 0, 0, 0]', ()=> {
-            expect(sort([0, 0, 0, 0])).eql([0, 0, 0, 0]);
+            expect(sort([0, 0, 0, 0])).to.eql([0, 0, 0, 0]);
         });
 
         it('Should sort [0, 3, 0]', ()=> {
-            expect(sort([0, 3, 0])).eql([0, 0, 3]);
+            expect(sort([0, 3, 0])).to.eql([0, 0, 3]);
         });
     });
 }
